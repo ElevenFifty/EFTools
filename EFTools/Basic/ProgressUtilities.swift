53d179d8342875416c713799c1f66daac5824faa
Brett Keck <bkeck@elevenfiftyconsulting.com>
2015-07-31 09:39:55 -0400

    Making things public, you know, just in case people want to use things

--- EFTools/Basic/ProgressUtilities.swift
+++ EFTools/Basic/ProgressUtilities.swift
@@ -8,11 +8,11 @@
 
 import Foundation
 
-class ProgressUtilities {
+public class ProgressUtilities {
     
     private static var hud : MBProgressHUD?
     
-    class func showSpinner(superView : UIView) {
+    public class func showSpinner(superView : UIView) {
         if let thisHud = hud {
             thisHud.hide(false)
             hud = nil
@@ -20,7 +20,7 @@ class ProgressUtilities {
         hud = MBProgressHUD.showHUDAddedTo(superView, animated: true)
     }
     
-    class func showSpinner(superView : UIView, title : String) {
+    public class func showSpinner(superView : UIView, title : String) {
         if let thisHud = hud {
             thisHud.hide(false)
             hud = nil
@@ -29,13 +29,13 @@ class ProgressUtilities {
         hud!.labelText = title
     }
     
-    class func hideSpinner() {
+    public class func hideSpinner() {
         if let thisHud = hud {
             thisHud.hide(true)
         }
     }
     
-    class func getHud() -> MBProgressHUD? {
+    public class func getHud() -> MBProgressHUD? {
         return hud
     }
 }
