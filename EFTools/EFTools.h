c658e24cdd8f73ff69c2391beadc0d6f55dd44ca
Brett Keck <bkeck@elevenfiftyconsulting.com>
2015-10-27 18:22:12 -0400

    Another test

--- EFTools/EFTools.h
+++ EFTools/EFTools.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char EFToolsVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <EFTools/PublicHeader.h>
 
 #import <MBProgressHUD/MBProgressHUD.h>
+#import <ParseUI/ParseUI.h>
