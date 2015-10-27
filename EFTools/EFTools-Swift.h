1671e8c98faacb996ba65a5d7d6eea75502594d2
Brett Keck <bkeck@elevenfiftyconsulting.com>
2015-10-27 17:28:28 -0400

    More fix tests

---
+++ EFTools/EFTools-Swift.h
@@ -0,0 +1,13 @@
+//
+//  EFTools-Swift.h
+//  EFTools
+//
+//  Created by Brett Keck on 10/27/15.
+//  Copyright © 2015 Brett Keck. All rights reserved.
+//
+
+#ifndef EFTools_Swift_h
+#define EFTools_Swift_h
+
+#import <MBProgressHUD/MBProgressHUD.h>
+#endif /* EFTools_Swift_h */
